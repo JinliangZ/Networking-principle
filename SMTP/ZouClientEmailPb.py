29e8d7784d8a2de7e452db76457ca11709207b8a
JinliangZ <33104348+JinliangZ@users.noreply.github.com>
2017-11-10 17:59:28 -0500

    Add files via upload

---
+++ SMTP/ZouClientEmailPb.py
@@ -0,0 +1,116 @@
+from socket import *
+import ssl
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header 
+from email.mime.image import MIMEImage
+
+msg = "\r\n I love computer networks!"
+endmsg = "\r\n.\r\n"
+
+# Choose a mail server (e.g. Google mail server) and call it mailserver
+mailserver = 'smtp.gmail.com'
+
+# Create socket called clientSocket and establish a TCP connection with mailserver 
+clientSocket = socket(AF_INET, SOCK_STREAM)
+#use ssl to secure transaction
+sslSocket = ssl.wrap_socket(clientSocket)
+
+# Create socket called sslSocket and establish a TCP connection with mailserver 
+sslSocket.connect((mailserver, 465))
+recv = sslSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '220':
+    print('220 reply not received from server.')
+
+# Send HELO command and print server response.
+heloCommand = 'HELO Alice\r\n'
+sslSocket.send(heloCommand.encode())
+recv1 = sslSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
+# start login and print server response.
+sslSocket.send(b"AUTH LOGIN\r\n")
+recv2 = sslSocket.recv(1024).decode()
+print(recv2)
+if recv2[:3] != '334':
+    print('334 reply not received from server.')
+
+# send username and print server response.
+#replace username with your email address
+username = 'bylearner@gmail.com'
+user64 = base64.b64encode(username.encode())
+sslSocket.send(user64+b'\r\n')
+recv3 = sslSocket.recv(1024).decode()
+print(recv3)
+if recv3[:3] != '334':
+    print('334 reply not received from server.')
+
+# send password and print server response.
+#replace password with your email password
+password = '*****************'
+pass64 = base64.b64encode(password.encode())
+sslSocket.send(pass64+b'\r\n')
+recv4 = sslSocket.recv(1024).decode()
+print(recv4)
+if recv4[:3] != '235':
+    print('235 reply not received from server.')
+
+# Send MAIL FROM command and print server response.
+MAIL_From = 'Mail From: <bylearner@gmail.com> \r\n'
+sslSocket.send(MAIL_From.encode())
+recv5 = sslSocket.recv(1024).decode()
+print(recv5)
+if recv5[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send RCPT TO command and print server response.
+RCPT_To = 'RCPT To: <jzou44@uwo.ca> \r\n'
+sslSocket.send(RCPT_To.encode())
+recv6 = sslSocket.recv(1024).decode()
+print(recv6)
+if recv6[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send DATA command and print server response.
+DATA = 'DATA\r\n'
+sslSocket.send(DATA.encode())
+recv7 = sslSocket.recv(1024).decode()
+print(recv7)
+if recv7[:3] != '354':
+    print('354 reply not received from server.')
+
+#get mail content
+f = open('picture1.png', 'rb')
+pic = MIMEImage(f.read())
+pic.add_header('Content-ID', '<image1>')
+f.close()
+msg_html = MIMEText('<b>I love computer networks!</b><br><img src="cid:image1"><br>', 'html')
+multi = MIMEMultipart('related')
+mul_alt = MIMEMultipart('alternative')
+multi.attach(mul_alt)
+mul_alt.attach(msg_html)
+multi.attach(pic)
+
+# Send message data.
+sslSocket.send(b'From: bylearner@gmail.com\r\n')
+sslSocket.send(b'To: jzou44@uwo.ca\r\n')
+sslSocket.send(b'Subject: question (b)\r\n')
+sslSocket.send(bytes(multi))
+
+# Message ends with a single period.
+sslSocket.send(endmsg.encode())
+
+# Send QUIT command and get server response.
+QUIT = 'QUIT\r\n'
+sslSocket.send(QUIT.encode())
+recv8 = sslSocket.recv(1024).decode()
+print(recv8)
+if recv8[:3] != '250':
+    print('250 reply not received from server.')
+
+clientSocket.close()
+sslSocket.close()
---
+++ SMTP/ZouClientEmailPc.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header 
+from email.mime.image import MIMEImage
+
+fromaddr = 'bylearner@gmail.com'
+toaddrs  = 'jzou44@uwo.ca'
+username = 'bylearner@gmail.com'
+password = '*****************'
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.login(username,password)
+server.set_debuglevel(True) # show communication with the server
+
+#get mail content
+f = open('picture1.png', 'rb')
+pic = MIMEImage(f.read())
+pic.add_header('Content-ID', '<image1>')
+f.close()
+msg_html = MIMEText('<b>I love computer networks!</b><br><img src="cid:image1"><br>', 'html')
+multi = MIMEMultipart('related')
+multi['Subject'] = 'question (c)'
+multi['From'] = 'bylearner@gmail.com'
+multi['To'] = 'jzou44@uwo.ca'
+mul_alt = MIMEMultipart('alternative')
+multi.attach(mul_alt)
+mul_alt.attach(msg_html)
+multi.attach(pic)
+
+server.sendmail(fromaddr, [toaddrs], multi.as_string())
+
+server.quit()
